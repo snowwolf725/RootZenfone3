054ec8b86f4fb4ece6368c34d260e8378e04789c
snowwolf725 <snowwolf725@gmail.comn>
2016-10-01 17:53:38 +0800

    Fix encode bug in script

--- 1.FlashRecovery.bat
+++ 1.FlashRecovery.bat
@@ -3,19 +3,11 @@ CLS
 COLOR 17
 ECHO ###############################################################################
 ECHO #                                                                             #
-ECHO #                   請將手機切換到 Bootloader 模式並連接電腦                  #
-ECHO #                                                                             #
-ECHO #                                                                             #
-ECHO #                                                                             #
 ECHO #  Please boot your mobile to Bootloader mode, and connect your mobile to PC  #
 ECHO #                                                                             #
 ECHO ###############################################################################
 fastboot flash recovery twrp-3.0.2-Z017D.img
 ECHO ###############################################################################
-ECHO #                                                                             #
-ECHO #               請按住手機音量小鍵, 並按下任意鍵                              #
-ECHO #                                                                             #
-ECHO #                                                                             #
 ECHO #                                                                             #
 ECHO #              Hold the volume down button, and press any key                 #
 ECHO #                                                                             #
