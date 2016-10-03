74f783e769ae665f03d1f76fca26b85b919928c1
snowwolf725 <snowwolf725@gmail.comn>
2016-10-03 10:47:35 +0800

    Add un-official bootloader unlock tool

---
+++ 0.unlock_ze520kl.bat
@@ -0,0 +1,32 @@
+@ECHO off
+CLS
+COLOR 17
+ECHO ###############################################################################
+ECHO #                                                                             #
+ECHO #  Please boot your mobile to Fastboot mode, and connect your mobile to PC    #
+ECHO #                                                                             #
+ECHO ###############################################################################
+
+pause
+
+ECHO ###############################################################################
+ECHO #                                                                             #
+ECHO #                     Press any to unlock device                              #
+ECHO #                                                                             #
+ECHO ###############################################################################
+
+pause
+
+fastboot flash all unlock_ze520kl.raw
+fastboot reboot-bootloader
+fastboot oem unlock-go
+
+ECHO ###############################################################################
+ECHO #                                                                             #
+ECHO #                           Device Unlocked                                   #
+ECHO #                                                                             #
+ECHO ###############################################################################
+
+fastboot oem device-info
+
+pause
